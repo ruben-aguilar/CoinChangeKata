931cc4ceecf57aefdb7a1a6428615011dc79d789
Ruben Aguilar Becerra <raguilarbecerra@vistaprint.com>
2018-04-18 13:55:28 +0200

    Input: 10, CoinDenomination: [2], expected: [5]

--- CoinChangerKata/CoinChanger.cs
+++ CoinChangerKata/CoinChanger.cs
@@ -1,16 +1,10 @@
 ﻿namespace CoinChangerKata
 {
-    internal class CoinChanger
+    internal static class CoinChanger
     {
-        public int[] GetChange(int input, int[] coinDenomination)
+        public static int[] GetChange(int input, int[] coinDenomination)
         {
-            if (input == 5 && coinDenomination[0] == 5)
-                return new[] {1};
-
-            if (input == 10)
-                return new[] {2};
-
-            return new[] { input };
+            return new[] { input/ coinDenomination[0] };
         }
     }
 }
--- CoinChangerKata/CoinChangerShould.cs
+++ CoinChangerKata/CoinChangerShould.cs
@@ -8,11 +8,13 @@ namespace CoinChangerKata
         [TestCase(1, new[]{1}, new[]{1})]
         [TestCase(2, new[]{1}, new[]{2})]
         [TestCase(5, new[]{1}, new[]{5})]
+        [TestCase(10, new[]{2}, new[]{5})]
         [TestCase(5, new[]{5}, new[]{1})]
         [TestCase(10, new[]{5}, new[]{2})]
+        [TestCase(3, new[]{1,2}, new[]{1,1})]
         public void return_coin_change(int input, int[] coinDenomination, int[] expected)
         {
-            int[] result = new CoinChanger().GetChange(input, coinDenomination);
+            int[] result = CoinChanger.GetChange(input, coinDenomination);
 
             Assert.AreEqual(expected, result);
         }
