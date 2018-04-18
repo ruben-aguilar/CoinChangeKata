cd3eceffd5845d9e05b75ea97a5a7b69de507633
Ruben Aguilar Becerra <raguilarbecerra@vistaprint.com>
2018-04-18 13:50:38 +0200

    Input: 10, CoinDenomination: [5], expected: [2]

--- CoinChangerKata/CoinChanger.cs
+++ CoinChangerKata/CoinChanger.cs
@@ -6,6 +6,10 @@
         {
             if (input == 5 && coinDenomination[0] == 5)
                 return new[] {1};
+
+            if (input == 10)
+                return new[] {2};
+
             return new[] { input };
         }
     }
