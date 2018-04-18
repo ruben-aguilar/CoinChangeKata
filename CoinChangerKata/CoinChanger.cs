cb7efcf92fcea3a6607be6dbf87b12162c4c1673
Ruben Aguilar Becerra <raguilarbecerra@vistaprint.com>
2018-04-18 13:25:50 +0200

    return_coin_change

---
+++ CoinChangerKata/CoinChanger.cs
@@ -0,0 +1,10 @@
+﻿namespace CoinChangerKata
+{
+    internal class CoinChanger
+    {
+        public int[] GetChange(int i, int[] coinDenomination)
+        {
+            return new int[]{1};
+        }
+    }
+}
---
+++ CoinChangerKata/CoinChangerShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CoinChangerKata
+{
+    [TestFixture]
+    internal class CoinChangerShould
+    {
+        [Test]
+        public void return_coin_change()
+        {
+            var coinDenomination = new[] {1};
+
+            int[] result = new CoinChanger().GetChange(1, coinDenomination);
+
+            Assert.AreEqual(new int[]{1}, result);
+        }
+    }
+}
