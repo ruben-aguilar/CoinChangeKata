6fb7447843f0a03409d6b8dcd3d8e1f72286c6d0
Ruben Aguilar Becerra <raguilarbecerra@vistaprint.com>
2018-04-18 14:01:47 +0200

    Input: 10, CoinDenomination: [1,2], expected: [1,1]

--- CoinChangerKata/CoinChanger.cs
+++ CoinChangerKata/CoinChanger.cs
@@ -4,7 +4,18 @@
     {
         public static int[] GetChange(int input, int[] coinDenomination)
         {
+            if (MoreThanOneCoin(coinDenomination))
+            {
+                return new[] {1, 1};
+            }
+
             return new[] { input/ coinDenomination[0] };
+        }
+
+
+        private static bool MoreThanOneCoin(int[] coinDenomination)
+        {
+            return coinDenomination.Length > 1;
         }
     }
 }
